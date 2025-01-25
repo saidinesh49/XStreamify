8d0ef6262a6163955c7f96c760cda30ce929516f
PALISETTY SAI DINESH <143427352+saidinesh49@users.noreply.github.com>
2025-01-25 23:47:17 +0530

    on fixing deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+export default defineConfig(({ mode }) => {
+	// Load environment variables based on the current mode
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [react()],
+		optimizeDeps: {
+			exclude: ["lucide-react"],
+		},
+		define: {
+			"process.env": { ...env }, // Make sure environment variables are accessible
+		},
+	};
 });
